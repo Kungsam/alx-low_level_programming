f786b0ab4473e03f126b1004ca7db15a87642469
Kungsam <snazzyhokey08@gmail.com>
2023-10-06 08:56:35 -0700

    my

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h> 
+
 /**
-*main - main entry point
-*return : always 0 (success)
+* main - this is the main function
+*
+* Return: Always 0
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
-
