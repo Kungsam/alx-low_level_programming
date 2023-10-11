deae70704419ee87b83ac26d557e20191f32e455
Kungsam <snazzyhokey08@gmail.com>
2023-10-11 10:25:23 -0700

    my

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 /**
   * _abs - Computes the absolute value of number or zero
-  * @0: The number to be computed.
+  * @c: The number to be computed.
   * Return: Absolute value of number or zero
   */
 
