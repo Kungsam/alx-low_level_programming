77a3a4695e2b1e37858527ab47c9b2628e6fba43
Kungsam <snazzyhokey08@gmail.com>
2023-11-06 20:03:32 -0800

    my

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#inlcude "dog.h"
+
+/**
+  * free_dog - frees nnory allocated for a struct dog
+  * @d: struct dog to free
+  */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
