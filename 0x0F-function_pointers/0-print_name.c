c8e307b82cd3a57ebc9d6b81bca7cac1ffdbee17
Kungsam <snazzyhokey08@gmail.com>
2023-11-08 12:50:56 -0800

    my

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
   * print_name - print name using pointer to function
   * @name: string to add
   * @f: pointer to function
-  * Retunrrn: nothing
+  * Return: nothing
   */
 void print_name(char *name, vpid (*f)(char *))
 {
